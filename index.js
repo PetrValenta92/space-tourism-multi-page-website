98b5c6cafe4a52a5ce98482cdc83082e998eee2b
Petr Valenta <petr.valenta.92@gmail.com>
2022-10-18 07:34:18 +0200

    Menu animation edited and close button added

--- index.js
+++ index.js
@@ -3,18 +3,16 @@ const navToggle = document.querySelector(".mobile-nav-toggle");
 
 // when someone clicks the humburger button
 navToggle.addEventListener("click", () => {
-
     
     const visiblity = nav.getAttribute("data-visible");
-    // if the nav is closed, open it
     if (visiblity === "false") {
         nav.setAttribute("data-visible", true);
+        navToggle.setAttribute("aria-expanded", true);
     }   else {
         nav.setAttribute("data-visible", false);
+        navToggle.setAttribute("aria-expanded", false);
     }
 
-    console.log(visiblity);
-
-    // if the nav is open, close it
+    console.log(navToggle.getAttribute("aria-expanded"));
 })
 
